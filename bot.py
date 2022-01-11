e0901b2914a1be8935d7cf01d638323898b61983
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-01-11 11:28:21 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ tickers = request["tickers"]
 
 _startTime = t.datetime.now()
 stocks = [Stock(tick) for tick in tickers]
-deltaTimeTotal = (t.datetime.now() - _startTime).total_seconds()
-del _startTime
 
 # Save Data as JSON
 data = {stock.ticker: stock.Dict() for stock in stocks}
+deltaTimeTotal = (t.datetime.now() - _startTime).total_seconds()
+del _startTime
 data.update({"deltaTimeTotal": deltaTimeTotal})
 jsonString = json.dumps(data, indent=4)
 with open("data.json", "w") as file: file.write(jsonString)
