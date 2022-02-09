651ade3c323ae2a1bf3af552906ddf04fa798414
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-02-09 13:21:59 +0200

    Add files via upload

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,22 +2,34 @@ import numpy as np, nmath
 from numpy import shape
 
 class NeuralNetwork:
-    def __init__(self, W):
+    def __init__(self, H):
         
         """Set a neural network"""
+        
+        self.layersLength = len(H)
+        
+        # Random Weights
+        self.W = [None for _ in range(self.layersLength)]
+        
+        for index in range(1, self.layersLength):
+            self.W[index] = np.random.randn(H[index-1], H[index])
+        
+        # Activation and "Semiactivation" Matrices
+        self.A = [None for _ in range(self.layersLength)]
+        self.Z = [None for _ in range(self.layersLength)]
+    
+    def ImportWeight(self, W):
+        
+        """"""
         
         if type(W) is list: self.W = W
         else: TypeError("StockBot Neural Module: Weight input should be a list")
         
         for _, this in enumerate(self.W):
             if type(this) is not np.ndarray: TypeError(f"StockBot Neural Module: {this} should be a numpy matrix")
-        
-        self.layersLength = len(self.W)
-        
-        # Activation and "Semiactivation" Matrices
-        self.A = [None for _ in range(self.layersLength)]
-        self.Z = [None for _ in range(self.layersLength)]
-        
+            
+        self.W = W
+    
     def Forward(self, X):
         
         """Forward Propagate (return output data)"""
@@ -27,17 +39,11 @@ class NeuralNetwork:
         # Z{n} = W{n} A{n-1}
         # A{n} = σ( Z{n} )
         
-        # Cheker
-        if X.shape != self.W[0].shape: TypeError("StockBot Neural Module: Input matrix and weight matrix should have the same dimensionality")
-        
-        # Normalize
-        X = X / np.max(X, axis=0)
-        
         # Compute Matrices
         self.A[0] = X
 
         for index in range(1, self.layersLength):
-            self.Z[index] = np.dot(self.W[index], self.A[index-1])
+            self.Z[index] = np.dot(self.A[index-1], self.W[index])
             self.A[index] = nmath.sigmoid(self.Z[index])
 
         # Return
@@ -49,9 +55,6 @@ class NeuralNetwork:
         
         # Error Math:
         # err = A{-1} - R
-        
-        # Normalize
-        R = R / np.max(R, axis=0)
         
         # Compute Errors
         return self.A[-1] - R
@@ -65,26 +68,42 @@ class NeuralNetwork:
         
         return sum(self.Error(R)**2)
         
-    
-    def Backward(self, R):
+    def Optimisation(self, R):
         
-        """Backpropagation (train neural network)"""
+        """"""
         
         # Backward Math:
         # δ{-1} = (A{-1} - R) o σ'( Z{-1} )
         # δ{n} = W*{n+1} δ{n+1} o σ'( Z{n} )
         # ΔW{n} = - δ{n} A*{n-1}
         
-        # Normalize
-        R = R / np.max(R, axis=0)
+        learrate = np.e
         
-        # Delta Matrices
+        # Initiate Matrices
         delta = [None for _ in range(self.layersLength)]
         delta[-1] = np.multiply(self.A[-1] - R, nmath.sigmoidprime(self.Z[-1]))
         
-        for index in range(self.layersLength-1, 0, -1):
-            delta[index] = np.dot(delta[index+1], self.W[index].T)
+        deltaW = [None for _ in range(self.layersLength)]
+        deltaW[-1] = -np.dot(self.A[-1].T, delta[-1])
+        
+        # Delta Matrices
+        for index in range(self.layersLength-2, 0, -1):
+            delta[index] = np.dot(delta[index+1], self.W[index+1].T) * nmath.sigmoidprime(self.Z[index])
+            
+        # Optimisation
+        deltaW = [None for _ in range(self.layersLength)]
+        for index in range(1, self.layersLength-1):
+            deltaW[index] = - learrate * np.dot(self.A[index].T, delta[index])
+            
+        return deltaW
+    
+    def Backward(self, R):
+        
+        """Backpropagation (train neural network)"""
+        
+        deltaW = self.Optimisation(R)
         
         # Update Weights
         for index in range(1, self.layersLength):
-            self.W[index] = self.W[index] - np.dot(self.A[index-1].T, delta[index])
+            if deltaW[index] is not None:
+                self.W[index] = self.W[index] + deltaW[index]
