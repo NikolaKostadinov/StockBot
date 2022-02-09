792d83816abe53f424f0447c66d74d8e0c96f6bd
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-02-09 17:56:04 +0200

    Add files via upload

--- speculate.py
+++ speculate.py
@@ -2,6 +2,30 @@ import bravotime, json, nmath
 from neuralnetwork import *
 from termcolor import colored
 
+def New(dataArray, ticker, id):
+    
+    """Generate new instance in ai.json"""
+    
+    # Checker
+    if type(dataArray) is not list: raise TypeError("StockBot Speculate Module: Input should be a list")
+    if type(ticker) is not str: raise TypeError("StockBot Speculate Module: Input should be a string")
+    
+    # Set Layers
+    L = [1, 3, 9, 9, 3, 1/24]
+    L = [int(len(dataArray) * l) for l in L]
+    n = len(L)
+
+    # Generate New Weights
+    wDict = {"0": None}
+    wDict.update({str(index): np.zeros(shape=(L[index-1], L[index])).tolist() for index in range(1, n)})
+    
+    # Convert to JSON
+    newDict = json.load(open("ai.json"))
+    newDict.update({ticker: {str(id): wDict}})
+    
+    jsonString = json.dumps(newDict, indent=4)
+    with open("ai.json", "w") as file: file.write(jsonString)
+
 def Spec(dataArray, ticker, id):
     
     """Speculate how an array will evolve with provided ticker and id"""
@@ -14,10 +38,16 @@ def Spec(dataArray, ticker, id):
     L = [1, 3, 9, 9, 3, 1/24]
     L = [int(len(dataArray) * l) for l in L]
     
-    W = None # Depends on <ai.json>
+    try: json.load(open("ai.json"))[ticker][id]
+    except KeyError: New(dataArray, ticker, id)
+    
+    weightData = json.load(open("ai.json"))[ticker][str(id)]
+    W = [None for _ in weightData]
+    for index in weightData:
+        W[int(index)] = np.array(weightData[index])
     
     X = np.array(dataArray)
-    N = np.maximum(X)
+    N = np.amax(X, axis=0)
     X = nmath.sigmoid(X / N)
     
     # A.I.
