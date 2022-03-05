15a904a8378ed05ab890086fe4ba07ab358c2df4
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-03-05 17:51:35 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,26 +1,39 @@
-import json, bravotime
+import json
+import bravotime
+import os
 from security import *
 from termcolor import colored
 
 # Start
 now = bravotime.NowString()
+print(colored("======================================", "blue"))
 print(colored(f"<{now}| StockBot is running", "yellow"))
 
 # Get Stocks
 tickers = json.load(open("request.json"))["tickers"]
 
 _startTime = bravotime.Now()
-security = [Security(tick) for tick in tickers]
+security = [Security(ticker=tick) for tick in tickers]
+security = list(map(lambda x: x.SpeculateUpdate(), security))
 
 # Save Data as JSON
-data = {this.ticker: this.Dict() for this in security}
+data = {secure.ticker: secure.Dict() for secure in security}
 deltaTimeTotal = (bravotime.Now() - _startTime).total_seconds()
 del _startTime
 data.update({"deltaTimeTotal": deltaTimeTotal})
 jsonString = json.dumps(data, indent=4)
 with open("data.json", "w") as file: file.write(jsonString)
 
+# Send Data to Server 0
+fileForTransfer = "data.json"
+toInstance = "instance-stockbot-0"
+
+os.system(f"gcloud compute scp {fileForTransfer} --zone=europe-west6-a {toInstance}:~")
+
+now = bravotime.NowString()
+print(colored(f"<{now}| Data uploaded to <{toInstance}>", "green"))
+
 # Console Feedback
 now = bravotime.NowString()
-print(colored(f"<{now}| StockBot is at rest", "blue"))
-print(colored(f"<{now}| Total time: {deltaTimeTotal}", "blue"))
+print(colored(f"<{now}| StockBot is at rest", "yellow"))
+print(colored(f"<{now}| Total time: {deltaTimeTotal}", "yellow"))
--- bravotime.py
+++ bravotime.py
@@ -1,4 +1,19 @@
 import datetime, pytz
 
-def Now(): return datetime.datetime.now(tz=pytz.timezone("Europe/Sofia"))
-def NowString(): return datetime.datetime.now(tz=pytz.timezone("Europe/Sofia")).strftime("%H:%M")
+def Now():
+    
+    """Return date in bravo time zone as datetime"""
+    
+    return datetime.datetime.now(tz=pytz.timezone("Europe/Sofia"))
+
+def NowString():
+    
+    """Return date in bravo time zone as a string"""
+    
+    return Now().strftime("%H:%M")
+
+def Convert(date):
+    
+    """Convert any date to bravo time zone datetime"""
+    
+    return date.astimezone(tz=pytz.timezone("Europe/Sofia"))
