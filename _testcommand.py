b85a7503506ed4f9a755da14a377e2809709f4dc
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-03-16 11:24:41 +0200

    Delete _testcommand.py

--- _testcommand.py
+++
@@ -1,7 +0,0 @@
-import os
-
-fileForTransfer = "_testcommand.py"
-toInstance = "instance-stockbot-0"
-command = f"gcloud compute scp {fileForTransfer} --zone=europe-west6-a {toInstance}:~"
-
-os.system(command)
