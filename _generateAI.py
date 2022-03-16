1574ea78858520b5232428cb44de13255454ac50
Nikola Kostadinov <93470986+NikolaKostadinov@users.noreply.github.com>
2022-03-16 11:22:20 +0200

    Delete _generateAI.py

--- _generateAI.py
+++
@@ -1,8 +0,0 @@
-import speculate
-import random
-from termcolor import colored
-
-dataArray = [random.uniform(0, 1) for _ in range(240)]
-speculate.New(dataArray)
-
-print(colored(":> Ready", "green"))
